1c9e5f336b2d5c7cd36c11ad0afe86e722d12510
hyunbin1 <kimgusqls1@gmail.com>
2020-08-02 11:55:26 +0900

    create css

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Jasoseol
 # Register your models here.
+
+# 자소설 모델 페이지에 띄우기(등록하기)
+admin.site.register(Jasoseol)
+
--- main/models.py
+++ main/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Jasoseol(models.Model):
     title = models.CharField(max_length=50)
-content = models.TextField()
-update_at = models.DateTimeField(auto_now=True) 
+    content = models.TextField(null=True)
+    update_at = models.DateTimeField(auto_now=True) 
