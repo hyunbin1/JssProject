8bf0aa198a95352cb395155fec2fbccaf73f241c
hyunbin1 <kimgusqls1@gmail.com>
2020-08-04 17:18:26 +0900

    customize django login form

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
-from .views import signup
+from .views import signup , MyLoginView
 
+# from django.contrib.auth.views import LoginView, LogoutView - 로그인 커스텀 안할때 
+# 로그인 커스텀 할때 LoginView 삭제해줌
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('signup/', signup, name="signup"),
+    # path('login/', login, name="login"), 이렇게 입력하지 않고 장고에서 제공하는 로그인 클레스를 사용한다.
+    # class를 뷰에서 직접 실행하고 싶으면 .as_view를 써줘야 된다. 
+
     
-]
+    # path('login/' ,LoginView.as_view(), name="login"), - 기존 장고에 있는 폼 가져오는 법
+    # 커스텀 해준 로그인 뷰 가져오는법
+    path('login/' ,MyLoginView.as_view(), name="login"),
+    path('logout/',LogoutView.as_view(), name="logout"),
+]  
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+# contrib.auth가 유저 모델 관련된 것들이 있다고 생각하면 된다. 
+from django.contrib.auth.forms import UserCreationForm
+# 로그인 기능 커스텀 하는법
+from django.contrib.auth.views import LoginView
 # Create your views here.
 
 # 회원가입 함수 만들기
 def signup(request):
+    # 회원가입 폼 만들어주기 - 유저모델이 만들어져 있고, 장고에서 다 제공을 해줌
+    # UsercCreationForm - 만들어져 있는 유저 폼 사용
+    regi_form = UserCreationForm()
+    # request post일 때 여기로 다시 돌아옴
+    if request.method == "POST":
+        filled_form = UserCreationForm(request.POST)
+        if filled_form.is_valid():
+            filled_form.save()
+            return redirect('index') 
 
-    return render(request, 'signup.html')
+    return render(request, 'signup.html', {'regi_form':regi_form})
+
+# 로그인 기능 커스텀 하는법
+class MyLoginView(LoginView):
+    template_name = 'login.html'
